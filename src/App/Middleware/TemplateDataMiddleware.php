b387b46aae44fe79760df29a8ddee6b5bc869802
MinaxiSokhad <minaxisokhad@gmail.com>
2024-09-04 12:26:59 +0530

    Add Dashboard Elemets

--- src/App/Middleware/TemplateDataMiddleware.php
+++ src/App/Middleware/TemplateDataMiddleware.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 namespace App\Middleware;
 
-use App\Services\{ProfileService, ProjectService, TaskService};
+use App\Services\{ProfileService, ProjectService, TaskService, CustomerService};
 use Framework\Contracts\MiddlewareInterface;
 use Framework\TemplateEngine;
 
@@ -12,7 +12,8 @@ class TemplateDataMiddleware implements MiddlewareInterface
         private TemplateEngine $view,
         private ProfileService $profileService,
         private ProjectService $projectService,
-        private TaskService $taskService
+        private TaskService $taskService,
+        private CustomerService $customerService
     ) {
     }
     public function process(callable $next)
@@ -22,10 +23,22 @@ class TemplateDataMiddleware implements MiddlewareInterface
         if (array_key_exists('user', $_SESSION)) {
             $profile = $this->profileService->getUserProfile((int) $_SESSION['user']);
             $this->view->addGlobal('profile', $profile);
+            $user_count = $this->profileService->getUserProfile();
+            $this->view->addGlobal('user_count', $user_count);
             $project_status = $this->projectService->getProjectStatus();
             $this->view->addGlobal('project_status', $project_status);
             // $task_status = $this->taskService->getTaskStatus();
             // $this->view->addGlobal('task_status', $task_status);
+            $project = $this->projectService->getProject();
+            $this->view->addGlobal('project', $project);
+            $project_count = $this->projectService->getoneproject();
+            $this->view->addGlobal('project_count', $project_count);
+            $task = $this->taskService->getTask();
+            $this->view->addGlobal('task', $task);
+            $task_count = $this->taskService->getonetask();
+            $this->view->addGlobal('task_count', $task_count);
+            $customer_count = $this->customerService->getcustomers();
+            $this->view->addGlobal('customer_count', $customer_count);
         }
         $this->view->addGlobal('title', 'PMS');
         $next();
--- src/App/Services/ProfileService.php
+++ src/App/Services/ProfileService.php
@@ -1,22 +1,28 @@
 <?php
-declare(strict_types= 1);
+declare(strict_types=1);
 namespace App\Services;
 use App\Config\Paths;
 use Framework\Database;
 use Framework\Exceptions\ValidationException;
-class ProfileService{
-    public function __construct(private Database $db){
+class ProfileService
+{
+    public function __construct(private Database $db)
+    {
 
     }
-    
-    public function getUserProfile(int $id) {
-        return $this->db->query(
-            "SELECT * FROM user WHERE id=:id",
-            [
-                'id'=> $_SESSION['user']
-            ]
-        )->find();
 
+    public function getUserProfile(int $id = 0)
+    {
+        if (!empty($id)) {
+            return $this->db->query(
+                "SELECT * FROM user WHERE id=:id",
+                [
+                    'id' => $_SESSION['user']
+                ]
+            )->find();
+        } else {
+            return $this->db->query("SELECT * FROM user")->findAll();
+        }
         if (!isset($_SESSION['user'])) {
             die("User not found.");
         }
